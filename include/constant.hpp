667f5a599efd19329bf120c39baf185bb1e1db5c
astro-v <valentinmichel07200@gmail.com>
2023-02-21 16:18:02 +0100

    Update
    Create main and constant files.

---
+++ include/constant.hpp
@@ -0,0 +1,8 @@
+#ifndef __CONSTANT_HPP__
+#define __CONSTANT_HPP__
+
+#define NB_CASE_X 8
+#define NB_CASE_Y 8
+#define NB_BOMB 10
+
+#endif // __CONSTANT_HPP__
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+- Demineur -
+main.cpp
+Valentin Michel
+21/02/2023
+*/
+
+/* #### INCLUDE #### */
+#include <iostream>
+
+/* #### CORE #### */
+int main()
+{
+    return 0;
+}
